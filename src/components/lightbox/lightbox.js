58532aa330d9b2b6102218db8c6e1fd5bf2b2921
luke codewalker <arathorn089@arcor.de>
2020-03-07 23:59:38 +0100

    lightbox functional

--- src/components/lightbox/lightbox.js
+++ src/components/lightbox/lightbox.js
@@ -5,10 +5,21 @@ export default class Lightbox extends BaseComponent {
     super(root);
     this.thumbnail = null;
     this.fullview = null;
+    this.loadingDots = null;
+    this.src = '';
+    this.img = null;
   }
 
   openThumbnail() {
     this.fullview.classList.add('open');
+    if (!this.img) {
+      this.img = document.createElement('img');
+      this.img.addEventListener('load', () => {
+        this.loadingDots.classList.add('hidden');
+        this.fullview.appendChild(this.img);
+      });
+      this.img.src = this.src;
+    }
   }
 
   closeThumbnail() {
@@ -18,6 +29,8 @@ export default class Lightbox extends BaseComponent {
   init() {
     this.thumbnail = this.componentRoot.querySelector('.thumbnail');
     this.fullview = this.componentRoot.querySelector('.fullview');
+    this.loadingDots = this.componentRoot.querySelector('.loading-dots');
+    this.src = this.componentRoot.dataset.src;
 
     this.addEventListeners({
       '.thumbnail': {

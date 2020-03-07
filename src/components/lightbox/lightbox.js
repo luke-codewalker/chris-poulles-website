8460d001d4f079db8d37344743dda30749c07420
luke codewalker <arathorn089@arcor.de>
2020-03-07 11:56:53 +0100

    adding husky

--- src/components/lightbox/lightbox.js
+++ src/components/lightbox/lightbox.js
@@ -3,6 +3,8 @@ import BaseComponent from '../../lib/base-component';
 export default class Lightbox extends BaseComponent {
   init() {
     this.addEventListeners({});
+    // make some error
+ 
     console.log('Lightbox is ready!');
   }
 }
